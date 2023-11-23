8dbe4a395777f3d67c48086b2bbcc2d924c6fa50
baishali2502 <baishali2502@gmail.com>
2023-11-23 07:10:06 +0530

    EmployeeDailyWage

--- EmployeeWage/src/EmployeeWage/EmployeeWage.java
+++ EmployeeWage/src/EmployeeWage/EmployeeWage.java
@@ -1,8 +1,9 @@
 package EmployeeWage;
 
-public class EmployeeWage {
-	private static void checkAttendance() {
-
+public class EmployeeWage 
+{
+	private static void checkAttendance() 
+	{
         double attendanceValue = Math.random();
 
         if (attendanceValue > 0.5) {
@@ -11,11 +12,19 @@ public class EmployeeWage {
             System.out.println("Employee is absent.");
         }
     }
+	
+	private static void dailywage(int hoursWorked, double wagePerHour) 
+	{
+        double dailyWage = hoursWorked * wagePerHour;
+        System.out.println("Employee daily wage = "+dailyWage);
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
 		System.out.println("Checking Attendance :");
 		checkAttendance();
+		dailywage(8,20.0);
 	}
 	
 }
