526b70b8c467811056f58343769e53d44ed78cc4
baishali2502 <baishali2502@gmail.com>
2023-11-23 06:55:00 +0530

    Added Employee Attendance

--- EmployeeWage/src/EmployeeWage/EmployeeWage.java
+++ EmployeeWage/src/EmployeeWage/EmployeeWage.java
@@ -1,11 +1,21 @@
 package EmployeeWage;
 
 public class EmployeeWage {
+	private static void checkAttendance() {
 
+        double attendanceValue = Math.random();
+
+        if (attendanceValue > 0.5) {
+            System.out.println("Employee is present.");
+        } else {
+            System.out.println("Employee is absent.");
+        }
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
 		System.out.println("Checking Attendance :");
+		checkAttendance();
 	}
 	
 }
