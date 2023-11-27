279ae22b569cfefde7cb969f0b3c30ae832df1ee
baishali2502 <baishali2502@gmail.com>
2023-11-27 18:46:33 +0530

    Welcome to Employee Wage Computation Program

--- EmployeeWage/src/EmployeeWage/EmployeeWage.java
+++ EmployeeWage/src/EmployeeWage/EmployeeWage.java
@@ -1,10 +1,29 @@
 package EmployeeWage;
+import java.util.*;
 
-public class EmployeeWage {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation Program");
+class employee_wage
+{
+	double perHourWage;
+	int fullHours;
+	int partHours;
+	
+	employee_wage(double perHourWage,int fullHours,int partHours)
+	{
+		this.perHourWage=perHourWage;
+		this.fullHours = fullHours;
+		this.partHours = partHours;
 	}
+	
+}
 
+public class EmployeeWage 
+{
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Welcome to Employee Wage Computation Program\n");
+		employee_wage e = new employee_wage(20.0,8,4);
+		
+	}
+	
 }
