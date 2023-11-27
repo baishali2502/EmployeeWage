38606681a0a9af1fd09ae771c7411cd37267eca5
baishali2502 <baishali2502@gmail.com>
2023-11-27 19:26:04 +0530

    Added UC-6:Calculate wage for certain hours and days

--- EmployeeWage/src/EmployeeWage/EmployeeWage.java
+++ EmployeeWage/src/EmployeeWage/EmployeeWage.java
@@ -13,6 +13,14 @@ class employee_wage
 		this.fullHours = fullHours;
 		this.partHours = partHours;
 	}
+	
+	/*
+	 * @desc:This methid checks attendance of employee using Random function
+	 * 
+	 * @params:none
+	 * 
+	 * @returns:void
+	 */
 	void checkAttendance() 
 	{
         double attendanceValue = Math.random();
@@ -23,9 +31,86 @@ class employee_wage
             System.out.println("Employee is absent.");
         }
     }
+
+	/*
+	 * @desc:This method calculates dailywage of employee
+	 * 
+	 * @params:none
+	 * 
+	 * @returns:void
+	 */
+	void dailywage() 
+	{
+        double dailyWage = perHourWage*fullHours;
+        System.out.println("Employee daily wage = "+dailyWage);
+    }
+
+	/*
+	 * @desc:This method calculates part-time wage of employee
+	 * 
+	 * @params:none
+	 * 
+	 * @returns:void
+	 */
+	void partTimeWage()
+	{
+		 double partWage = partHours * perHourWage;
+	     System.out.println("Part-time Employee wage = "+partWage);
+	}
+
+	/*
+	 * @desc:This method calculates employee wage using switchcase
+	 * 
+	 * @params:none
+	 * 
+	 * @returns:void
+	 */
+	void calculateWage_usingSwitch() {
+
+        final int fullDayHours = 8;
+        final int partTimeHours = 4;
+        Random rand = new Random(); 
+        int hoursWorked =  rand.nextInt(8); 
+        switch (hoursWorked) {
+            case fullDayHours:
+                dailywage();
+                break;
+            case partTimeHours:
+            	partTimeWage();
+                break;
+            default:
+                System.out.println("Invalid hours worked.");
+        }
+    }
 	
-	
-	
+	/*
+	 * @desc:This method calculates monthly-wage of employee
+	 * 
+	 * @params:No. of days worked by employee
+	 * 
+	 * @returns:void
+	 */
+	void monthlywage(int days)
+	{
+		double amt =  fullHours*perHourWage*days;
+		System.out.println("Monthly wages = "+amt);
+	}
+
+	/*
+	 * @desc:This method calculate employee wage for a certain hours and days
+	 * 
+	 * @params: hours & days worked
+	 * 
+	 * @return:void
+	 */
+	void conditional_wage(int hoursWorked,int days)
+	{
+		double perDayWage = fullHours*perHourWage;
+		double ans1 = perHourWage*hoursWorked;
+		double ans2 = perDayWage*days;
+		System.out.println("Total wage for working "+hoursWorked+"hrs = "+ans1);
+		System.out.println("Total wage for working "+days+"days = "+ans2);		
+	}
 }
 
 public class EmployeeWage 
