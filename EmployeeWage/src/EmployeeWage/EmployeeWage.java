693f974b06ac50fe41579be3b5d1bb4a01a9d148
baishali2502 <baishali2502@gmail.com>
2023-11-27 18:53:14 +0530

    Added UC-1 : Check employee attendance

--- EmployeeWage/src/EmployeeWage/EmployeeWage.java
+++ EmployeeWage/src/EmployeeWage/EmployeeWage.java
@@ -1,8 +1,28 @@
 package EmployeeWage;
+import java.util.*;
 
-public class EmployeeWage {
-	private static void checkAttendance() {
-
+class employee_wage
+{
+	double perHourWage;
+	int fullHours;
+	int partHours;
+	
+	employee_wage(double perHourWage,int fullHours,int partHours)
+	{
+		this.perHourWage=perHourWage;
+		this.fullHours = fullHours;
+		this.partHours = partHours;
+	}
+	
+	/*
+	 * @desc:This methid checks attendance of employee using Random function
+	 * 
+	 * @params:none
+	 * 
+	 * @returns:void
+	 */
+	void checkAttendance() 
+	{
         double attendanceValue = Math.random();
 
         if (attendanceValue > 0.5) {
@@ -11,11 +31,30 @@ public class EmployeeWage {
             System.out.println("Employee is absent.");
         }
     }
+
+	/*
+	 * @desc:This method calculates dailywage of employee
+	 * 
+	 * @params:none
+	 * 
+	 * @returns:void
+	 */
+	void dailywage() 
+	{
+        double dailyWage = perHourWage*fullHours;
+        System.out.println("Employee daily wage = "+dailyWage);
+    }
+	
+}
+
+public class EmployeeWage 
+{
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation Program");
-		System.out.println("Checking Attendance :");
-		checkAttendance();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Welcome to Employee Wage Computation Program\n");
+		employee_wage e = new employee_wage(20.0,8,4);
+		
 	}
 	
 }
