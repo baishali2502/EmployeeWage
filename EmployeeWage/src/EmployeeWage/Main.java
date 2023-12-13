94ec6322e86a0cc87d10e2c9bb3dae656ea371a8
baishali2502 <baishali2502@gmail.com>
2023-12-13 20:51:01 +0530

    Added main function

---
+++ EmployeeWage/src/EmployeeWage/Main.java
@@ -0,0 +1,14 @@
+package EmployeeWage;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Welcome to Employee Wage Computation Program\n");
+		employee_wage e = new employee_wage(20.0,8,4);
+	}
+
+}
