b6ea771ea73c6686564c0a9ecc63570b68d638fd
baishali2502 <baishali2502@gmail.com>
2023-12-13 21:12:12 +0530

    Added UC-6:Calculate wage till condition

--- EmployeeWage/src/EmployeeWage/EmployeeWage.java
+++ EmployeeWage/src/EmployeeWage/EmployeeWage.java
@@ -113,14 +113,4 @@ class employee_wage
 	}
 }
 
-public class EmployeeWage 
-{
-	
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Welcome to Employee Wage Computation Program\n");
-		employee_wage e = new employee_wage(20.0,8,4);
-		
-	}
-	
-}
+
