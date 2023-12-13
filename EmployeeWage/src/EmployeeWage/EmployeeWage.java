e8e4e972ae65f4b0fe7197972257d54ab72cf613
baishali2502 <baishali2502@gmail.com>
2023-12-13 21:00:14 +0530

    Added UC1:Check employee attendance

--- EmployeeWage/src/EmployeeWage/EmployeeWage.java
+++ EmployeeWage/src/EmployeeWage/EmployeeWage.java
@@ -31,30 +31,7 @@ class employee_wage
             System.out.println("Employee is absent.");
         }
     }
-
-	/*
-	 * @desc:This method calculates dailywage of employee
-	 * 
-	 * @params:none
-	 * 
-	 * @returns:void
-	 */
-	void dailywage() 
-	{
-        double dailyWage = perHourWage*fullHours;
-        System.out.println("Employee daily wage = "+dailyWage);
-    }
 	
 }
 
-public class EmployeeWage 
-{
-	
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Welcome to Employee Wage Computation Program\n");
-		employee_wage e = new employee_wage(20.0,8,4);
-		
-	}
-	
-}
+
